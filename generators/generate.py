1d9fe0f71ebc55a4c8cece964b471de8d2e84930
ingako <hwu344@aucklanduni.ac.nz>
2021-08-11 12:23:13 +1200

    Update generate.py

--- generators/generate.py
+++ generators/generate.py
@@ -2,14 +2,16 @@
 
 import os
 from skmultiflow.data.random_tree_generator import RandomTreeGenerator
+from pathlib import Path
 
 num_sample = 10000
 seed = 42
 delim = ','
-output_filename = f'../data/tree/{seed}.arff'
 
+max_tree_depth=5
+min_leaf_depth=5
 n_cat_features = 5
-n_categories_per_cat_feature=2
+n_categories_per_cat_feature= 2 # fix
 n_classes = 2
 header = []
 
@@ -19,6 +21,9 @@ for i in range(n_cat_features * n_categories_per_cat_feature):
 header.append(f"@attribute class {{0,1}}")
 header.append("@data\n")
 
+output_dir = f'../data/tree/{n_cat_features}/{max_tree_depth}/{min_leaf_depth}'
+Path(output_dir).mkdir(parents=True, exist_ok=True)
+output_filename = f'{output_dir}/{seed}.arff'
 
 # Setting up the stream
 stream = RandomTreeGenerator(
@@ -27,8 +32,8 @@ stream = RandomTreeGenerator(
             n_cat_features=n_cat_features,
             n_num_features=0,
             n_categories_per_cat_feature=n_categories_per_cat_feature,
-            max_tree_depth=5,
-            min_leaf_depth=3,
+            max_tree_depth=max_tree_depth,
+            min_leaf_depth=min_leaf_depth,
             fraction_leaves_per_level=0.15)
 
 print(f'generating {output_filename}...')
