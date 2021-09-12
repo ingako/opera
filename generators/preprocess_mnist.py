f82317ca8f8016a86540a129af9fecfd5a409ec9
ingako <ingako@protonmail.com>
2021-09-13 11:44:22 +1200

    update preprocess_mnist.py

--- generators/preprocess_mnist.py
+++ generators/preprocess_mnist.py
@@ -5,20 +5,24 @@ import os
 import logging
 from pathlib import Path
 
-from skmultiflow.data.random_tree_generator import RandomTreeGenerator
 from stream_generator import RecurrentDriftStream
 import pandas as pd
 
 def is_unique(s):
-    a = s.to_numpy() # s.values (pandas<0.24)
+    a = s.to_numpy()
     return (a[0] == a).all()
 
-flip_percentage = 35
+# flip_digits = [0, 1]
+# flip_digits = [0, 1, 2, 3, 4]
+flip_digits = [0, 1, 2, 3, 4, 5, 6, 7]
 
-df = pd.read_csv("../data/mnist/mnist.csv", header=0)
+# df = pd.read_csv("../data/mnist/mnist.csv", header=0)
+df = pd.read_csv("../data/fashion-mnist/fashion-mnist.csv", header=0)
+
 # print(df.head())
 n_row, n_col = df.shape
 print(f"row={n_row}, col={n_col}")
+
 
 # remove useless attributes
 df_new = pd.DataFrame(df.iloc[:,0])
@@ -29,17 +33,21 @@ for i in range(1, n_col):
         count += 1
 
 df = df_new
-n_row, n_col = df.shape
 df_orig = df.copy()
-df_orig = df_orig.sample(frac=1) # shuffle
+df_orig = df_orig.sample(frac=1, random_state=0) # shuffle
 
-print(f"n_col={n_col}")
+n_row, n_col = df.shape
+print(f"row={n_row}, col={n_col}")
 
 
 # flip
-for i in range(1, int(flip_percentage/100 * n_col)):
-    df.iloc[:,i] = df.iloc[:,i].mul(-1).add(255)
-df = df.sample(frac=1) # shuffle
+rows_flip = df.label.isin(flip_digits).astype(bool)
+# print(f"row={rows_flip}")
+for col_name in list(df):
+    if col_name == "label": continue
+    df.loc[rows_flip, col_name] = df.loc[rows_flip, col_name].mul(-1).add(255)
+
+df = df.sample(frac=1, random_state=1) # shuffle
 
 # concat
 df = df_orig.append(df, ignore_index=True)
@@ -50,9 +58,10 @@ label_col = df.iloc[:,0].copy()
 df = df.drop("label", 1)
 df.insert(n_col-1, "label", label_col)
 
-output_dir = f'../data/mnist/'
+# output_dir = f'../data/mnist/'
+output_dir = f'../data/fashion-mnist/'
 Path(output_dir).mkdir(parents=True, exist_ok=True)
-output_filename = f'{output_dir}/flip{flip_percentage}.arff'
+output_filename = f'{output_dir}/flip{"".join([str(d) for d in flip_digits])}.arff'
 df.to_csv(output_filename, sep=',', header=False, index=False)
 
 
